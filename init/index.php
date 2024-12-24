e16c60047ccabb48a3d4e9411eda83faae1763da
Надежда <timakovanadezda0@gmail.com>
2024-12-24 20:19:04 +0300

    заполнение лотов списка lots__list из массива

--- init/index.php
+++ init/index.php
@@ -114,17 +114,20 @@ $lots = [
         </div>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
+            <?php foreach ($lots as $key => $value): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
+                    <img src="<?=$value['url'];?>" width="350" height="260" alt="<?=$value['lot'];?>">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
+                    <span class="lot__category"><?=$value['category'];?> </span>
+                    <h3 class="lot__title">
+                        <a class="text-link" href="pages/lot.html"><?=$value['lot'];?></a>
+                    </h3>
                     <div class="lot__state">
                         <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">цена<b class="rub">р</b></span>
+                            <span class="lot__amount"><?=$value['price']; //стартовая цена?> </span>
+                            <span class="lot__cost"><?=$value['price']; //текцщая цена?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -132,6 +135,7 @@ $lots = [
                     </div>
                 </div>
             </li>
+        <?php endforeach; ?>
         </ul>
     </section>
 </main>
