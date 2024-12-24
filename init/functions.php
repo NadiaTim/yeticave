0c136a71de6dbf14912447d441c2a615c70ad697
Надежда <timakovanadezda0@gmail.com>
2024-12-25 01:56:17 +0300

    Добавлена функция рассчета оставшейся жизни лота

--- init/functions.php
+++ init/functions.php
@@ -17,4 +17,33 @@ function price_format(int $price) : string {
     $price = number_format($price, 0,"."," ");
     $price = $price." "."₽";
     return $price;    
-}
+};
+
+/**
+ * Рассчитывает оставшееся количество часов и минут от введенной даты до текущей
+ * Примеры использования (текущая дата 2024-12-25 0:34):
+ * rest_time('2024-12-26'); //
+ * 
+ * 
+ * на вход функция принимает дату в формате ГГГГ-ММ-ДД;
+ * возвращает массив, где первый элемент — целое количество часов до даты, а второй — остаток в минутах.
+ * 
+ * 
+ */
+function rest_time($dateFin) {
+    $dateFin = strtotime($dateFin);
+    $dateNow = strtotime("now");
+    $diff    = $dateFin - $dateNow;
+    $days    = floor($diff/86400);
+    $hours   = floor($diff/3600) - $days*24;
+    $minutes = floor($diff/60) - $hours*60 - $days*24*60;
+
+    
+    $hours   = str_pad($hours, 2, "0", STR_PAD_LEFT);
+    $minutes = str_pad($minutes, 2, "0", STR_PAD_LEFT);
+    $arrRez[] = $days;
+    $arrRez[] = $hours;
+    $arrRez[] = $minutes;
+
+    return $arrRez;
+};
--- init/templates/main.php
+++ init/templates/main.php
@@ -32,8 +32,9 @@
                             <span class="lot__amount"><?=price_format($value['price']); //стартовая цена c ajhvfnbhjdfybtv?> </span>
                             <span class="lot__cost"><?=price_format($value['price']); //текущая цена с форматированием?></span>
                         </div>
+                        <?php $restTime=rest_time($value['dateFin']);?>
                         <div class="lot__timer timer">
-                            12:23
+                            <?="$restTime[0]"."д ". "$restTime[1]:$restTime[2]"?>
                         </div>
                     </div>
                 </div>
