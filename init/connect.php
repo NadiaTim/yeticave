147d245df30e362f586374989c6ea74ecf3a4e4e
Надежда Тимакова <131198459+NadiaTim@users.noreply.github.com>
2024-12-26 23:35:34 +0300

    Вывод данных для главной страницы через БД (вынос подключения к БД) (#4)
    * Подключение главной страницы к MySQL, установка соединения с БД
    
    * Изменено получение списка категорий на данные из БД
    
    * Изменено получение списка лотов на данные из БД
    
    * Выносим подключение к БД в отдельный файл

---
+++ init/connect.php
@@ -0,0 +1,4 @@
+<?php
+//подключение к БД
+$con = mysqli_connect("MySQL-5.7","root","","yeticave");
+mysqli_set_charset($con, "utf8");
--- init/index.php
+++ init/index.php
@@ -1,7 +1,40 @@
 <?php
 require 'functions.php';
-require 'helpers.php';
-require 'data.php';
+//содержит функцию для расчета оставшегося времени лота 
+//и форматирования суммы
+require 'helpers.php'; 
+//содержит функцию для построения страницы из шаблонов
+require 'data.php'; 
+//подключает: список категорий, авторизацию
+require 'connect.php';
+//содержит подключение к БД
+
+//получение массива лотов
+if (!$con) {
+    print("Ошибка подключения: ". mysqli_connect_error());
+    $error = mysqli_connect_error();
+} else {
+    $sql = "SELECT l.lot_id, l.name, l.start_price, l.image, COALESCE(p.price,l.start_price) fin_price, c.name category, l.finsh_date finish_date
+            FROM lots l
+            JOIN categories c 
+            ON l.category_id = c.category_id
+            LEFT JOIN ( SELECT lot_id, max(price) price
+                FROM bets
+                GROUP BY lot_id) p 
+            ON l.lot_id = p.lot_id
+            WHERE l.finsh_date>now()
+            ORDER BY l.create_date DESC;";
+    $result = mysqli_query($con, $sql);
+    if ($result) {
+        $lots = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    } else {
+        $error = mysqli_error($con);
+        print("Ошибка запроса: ".$error);
+    }
+}
+
+
+//вывод отображения страницы
 
 //подключение к БД
 $con = mysqli_connect("MySQL-5.7","root","","yeticave");
