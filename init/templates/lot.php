fa65f904652cc7d322c8d7867e5b3e232a6cb5a3
Надежда <timakovanadezda0@gmail.com>
2025-01-03 23:21:50 +0300

    Сокрытия блока добавления ставки на странице лота для неавторизированных пользователей

--- init/templates/lot.php
+++ init/templates/lot.php
@@ -11,6 +11,7 @@
           <p class="lot-item__description"><?=$lot['discription']; ?></p>
         </div>
         <div class="lot-item__right">
+        <?php if(isset($_SESSION['user'])): ?>
           <div class="lot-item__state ">
             <?php $restTime=rest_time($lot['finish_date']); ?>
             <div class="lot-item__timer timer <?=($restTime[0]<1)&&($restTime[1]<1)?"timer--finishing":"";?>">
@@ -39,6 +40,7 @@
               <button type="submit" class="button">Сделать ставку</button>
             </form>
           </div>
+        <?php endif; ?>
           <div class="history">
             <h3>История ставок (<span>10</span>)</h3>
             <table class="history__list">
