60093a6bb6b04fee47c4735939d02956588a42ab
Надежда <timakovanadezda0@gmail.com>
2025-01-03 22:55:57 +0300

    Реализация валидации формы, при успешной аудентификации запуск сессиии

--- init/login.php
+++ init/login.php
@@ -22,31 +22,43 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
 	$errors=[];
 
 	//проверяем поля на заполненность
-
-	//проверка на существование пользователя с введеным email
-
-	//если пользователь есть проверяем корректность его пароля
-	$passwordHash //хэш пароля из БД
-	if (!password_verify('bad-password', $passwordHash)) {
-		$errors['password'] = "Некорректный пароль";
-	}
-
-	if (count($errors)) {
-		$login = include_template('login.php', [
-			'categories_temp' => $categories_temp,
-			'errors'          => $errors,
-			'email'			  => $user['email']
-		]);
-	} else {
-		//начинаем сессию, переходим на главную страницу
-		$_SESSION['user'] = $user;
-		//подумать как перенаправить на страницу откуда пользователь пришел
-		header("Location: /index.php");
-		exit();
+	foreach ($user as $key => $value) {
+		if (empty($value)) {
+			$errors[$key] = "Это необходимо заполнить";
+		}
 	}
 	
 
+	if (!count($errors)){
+		//проверка на существование пользователя с введеным email
+		$sql = "SELECT * FROM users WHERE email = ?";
+		$stmt = mysqli_prepare($con, $sql);
+		mysqli_stmt_bind_param($stmt,'s', $user['email']);
+		mysqli_stmt_execute($stmt);
+		$res = mysqli_stmt_get_result($stmt);
+		//получаем данные пользователя из БД
+		$user_sql = $res?mysqli_fetch_array($res,MYSQLI_ASSOC):NULL;
 
+		if ($user_sql) {
+		//если пользователь есть проверяем корректность его пароля
+			if (password_verify($user['password'], $user_sql['password'])) {
+				//начинаем сессию, переходим на главную страницу
+				$_SESSION['user'] = $user;
+				//подумать как перенаправить на страницу откуда пользователь пришел
+				header("Location: /index.php");
+				exit();
+			} else {
+				$errors['password'] = "Введен некорректный пароль";
+			}
+		} else {
+			$errors['email'] = "Данный пользователь не существует";
+		}
+	}
+	$login = include_template('login.php', [
+		'categories_temp' => $categories_temp,
+		'errors'          => $errors,
+		'email'			  => $user['email']
+	]);
 } else {
 	//Стартовое состояние без отправки формы
 	$login = include_template('login.php', [
@@ -63,5 +75,6 @@ $layout = include_template('layout.php', $data = [
 		'main'            => $login
  ]);
 print($layout);
+print_r($errors);
 
 
