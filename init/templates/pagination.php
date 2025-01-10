37c3a485cf697ba2343f85d222d4c2b47c90b186
Надежда <timakovanadezda0@gmail.com>
2025-01-10 13:20:57 +0300

    Корректеровка функции пагинации: корректировка работы ссылок вперед/назад

--- init/templates/pagination.php
+++ init/templates/pagination.php
@@ -1,19 +1,23 @@
-<ul class="pagination-list">
-<?php 
-$url = $_SERVER['REQUEST_URI'];
-?>
-    <li class="pagination-item pagination-item-prev">
-        <a href="<?= str_contains($url, '?')?$url."&&page=".$cur_page-1:$url."?page=".$cur_page-1;?>">Назад</a>
-    </li>
-    <?php for ($i=1; $i < $page_count; $i++):
-        $url = stristr($url, '&&page', true);
-        $url = str_contains($url, '?')?$url."&&page=".$i:$url."?page=".$i; 
+<?php if ($page_count>1): ?>
+    <ul class="pagination-list">
+    <?php 
+    $url = $_SERVER['REQUEST_URI'];
+    $url0 = stristr($url, '&&page', true)?stristr($url, '&&page', true):$url;
+    $url0 = stristr($url0, '?page', true)?stristr($url0, '?page', true):$url0;;
     ?>
-    <li class="pagination-item <?= $i===$cur_page?"pagination-item-active":""; ?>">
-        <a href="<?= $url;?>"><?=$i;?></a>
-    </li>
-    <?php endfor; ?>
-    <li class="pagination-item pagination-item-next">
-        <a href="<?= str_contains($url, '?')?$url."&&page=".$cur_page+1:$url."?page=".$cur_page+1;?>">Вперед</a>
-    </li>
-</ul>
+        <li class="pagination-item pagination-item-prev">
+            <a href="<?= str_contains($url0, '?')?$url0."&&page=".$cur_page-1:$url0."?page=".$cur_page-1;?>">Назад</a>
+        </li>
+        <?php for ($i=1; $i <= $page_count; $i++):
+            $url = stristr($url, '&&page', true);
+            $url = str_contains($url, '?')?$url."&&page=".$i:$url."?page=".$i; 
+        ?>
+        <li class="pagination-item <?= $i===$cur_page?"pagination-item-active":""; ?>">
+            <a href="<?= $url;?>"><?=$i;?></a>
+        </li>
+        <?php endfor; ?>
+        <li class="pagination-item pagination-item-next">
+            <a href="<?= str_contains($url0, '?')?$url0."&&page=".$cur_page+1:$url0."?page=".$cur_page+1;?>">Вперед</a>
+        </li>
+    </ul>
+<?php endif;?>
